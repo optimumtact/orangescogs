971d23e140c9db5ceb4dd791ed525ba5ba24e889
oranges <email@oranges.net.nz>
2020-12-15 09:42:08 +0000

    Denton

--- pet/pets.py
+++ pet/pets.py
@@ -45,6 +45,25 @@ class Pets(BaseCog):
         " disappointingly lukewarm"
         ]
 
+        self.quotes = [
+            "My vision is augmented.",
+            "Well, since that makes you my new boss, take a long look at Manderley's dead body. Consider that my resignation; I don't have time to write a letter.",
+            "When due process fails us, we really do live in a world of terror.",
+            "You mechs may have copper wiring to reroute your fear of pain, but I've got nerves of steel.",
+            "I never had time to take the Oath of Service to the Coalition. How about this one? I swear not to rest until UNATCO is free of you and the other crooked bureaucrats who have perverted its mission.",
+            "Call me nostalgic, but the nightlife seems to have lost its old charm.",
+            "Some gang-banger, maybe you should think about going back to school.",
+            "Bravery is not a function of firepower.",
+            "Human beings may not be perfect, but a computer program with language synthesis is hardly the answer to the world's problems.",
+            "Every war is the result of a difference of opinion. Maybe the biggest questions can only be answered by the greatest of conflicts."
+            "What good's an honest soldier if he can be ordered to behave like a terrorist?",
+            "You've got ten seconds to beat it before I add you to the list of NSF casualties.",
+            "What a shame.",
+            "A forgotten virtue like honesty is worth at least twenty credits.",
+            "I'm not big into books.",
+            "I'm not going to stand here and listen to you badmouth the greatest democracy the world has ever known."
+        ]
+
     @commands.command()
     async def pet(self, ctx, *, name: str):
         """
@@ -72,6 +91,7 @@ class Pets(BaseCog):
         Nom
         """
         await ctx.send("*{} bites {}'s tail ferociously and tears it off completely*".format(ctx.author.mention, name))
+
     @commands.command(aliases=["taildestroy"])
     async def destroy(self, ctx, *, name: str):
         """
@@ -134,3 +154,10 @@ class Pets(BaseCog):
         You so, fucking FINE
         """
         await ctx.send("{} you are fined one credit for violation of the textual morality statutes".format(name))
+
+    @commands.command()
+    async def denton(self, ctx):
+        """
+        JC, A BOMB
+        """
+        await ctx.send(random.choice(self.quotes))
