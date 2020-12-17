a8326386b207086f71b71c5b2d58597acfd9c8d5
oranges <email@oranges.net.nz>
2020-12-17 06:53:56 +0000

    tgverify Fix command warning

--- tgverify/tgverify.py
+++ tgverify/tgverify.py
@@ -433,7 +433,7 @@ class TGverify(BaseCog):
                     reason="User has verified against their in game living minutes",
                 )
 
-            fuck = f"Congrats {ctx.author} your verification is complete, but you do not have {min_required_living_minutes} minutes in game as a living crew member (you have {player['living_time']}), so you may not have access to all channels. You can always verify again later by simply doing `[p]verify` and if you have enough minutes, you will gain access to the remaining channels"
+            fuck = f"Congrats {ctx.author} your verification is complete, but you do not have {min_required_living_minutes} minutes in game as a living crew member (you have {player['living_time']}), so you may not have access to all channels. You can always verify again later by simply doing `?verify` and if you have enough minutes, you will gain access to the remaining channels"
             if successful:
                 fuck = f"Congrats {ctx.author} your verification is complete"
             return await message.edit(content=fuck, color=0xFF0000)
